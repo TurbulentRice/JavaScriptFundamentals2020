5dcef82c3a1c8f662384838c6d336bcbb22f7b07
Sean Russell <seanrussellphotography@gmail.com>
2020-11-04 21:30:30 -0500

    promises hw

--- exercises/20-promise/promise.js
+++ exercises/20-promise/promise.js
@@ -5,30 +5,54 @@
  * Using the Promise API, create a fake AJAX request that should take 50 milliseconds to complete.
  * The promise should resolve into this response:
  * @example
- * {
- *  "category": "books",
- *  "subCategory": "graphic novels",
- *  "data": [{
- *    "id": "0c8cb1b2",
- *    "title": "Berlin",
- *    "authors": ["Jason Lutes"],
- *    "image": "https://images-na.ssl-images-amazon.com/images/I/51DhbDeVIVL._SX388_BO1,204,203,200_.jpg"
- *  },{
- *    "id": "cb195709",
- *    "title": "Hey, Kiddo",
- *    "authors": ["Jarrett J. Krosoczka"],
- *    "image": "https://images-na.ssl-images-amazon.com/images/I/517I7YRvHBL._SX351_BO1,204,203,200_.jpg"
- *  },{
- *    "id": "77ae31c1",
- *    "title": "On a Sunbeam",
- *    "authors": ["Tillie Walden"],
- *    "image": "https://images-na.ssl-images-amazon.com/images/I/51Ukxxbo-mL._SX359_BO1,204,203,200_.jpg"
- *  }]
- * }
+//  * {
+//  *  "category": "books",
+//  *  "subCategory": "graphic novels",
+//  *  "data": [{
+//  *    "id": "0c8cb1b2",
+//  *    "title": "Berlin",
+//  *    "authors": ["Jason Lutes"],
+//  *    "image": "https://images-na.ssl-images-amazon.com/images/I/51DhbDeVIVL._SX388_BO1,204,203,200_.jpg"
+//  *  },{
+//  *    "id": "cb195709",
+//  *    "title": "Hey, Kiddo",
+//  *    "authors": ["Jarrett J. Krosoczka"],
+//  *    "image": "https://images-na.ssl-images-amazon.com/images/I/517I7YRvHBL._SX351_BO1,204,203,200_.jpg"
+//  *  },{
+//  *    "id": "77ae31c1",
+//  *    "title": "On a Sunbeam",
+//  *    "authors": ["Tillie Walden"],
+//  *    "image": "https://images-na.ssl-images-amazon.com/images/I/51Ukxxbo-mL._SX359_BO1,204,203,200_.jpg"
+//  *  }]
+//  * }
  *
  * @returns {Promise} which will resolve to the JSON above.
  */
-const getBooksApi = () => {};
+const getBooksApi = () => new Promise((resolve, reject) => {
+  let success = true;
+  let response = {
+    "category": "books",
+    "subCategory": "graphic novels",
+    "data": [{
+      "id": "0c8cb1b2",
+      "title": "Berlin",
+      "authors": ["Jason Lutes"],
+      "image": "https://images-na.ssl-images-amazon.com/images/I/51DhbDeVIVL._SX388_BO1,204,203,200_.jpg"
+    },{
+      "id": "cb195709",
+      "title": "Hey, Kiddo",
+      "authors": ["Jarrett J. Krosoczka"],
+      "image": "https://images-na.ssl-images-amazon.com/images/I/517I7YRvHBL._SX351_BO1,204,203,200_.jpg"
+    },{
+      "id": "77ae31c1",
+      "title": "On a Sunbeam",
+      "authors": ["Tillie Walden"],
+      "image": "https://images-na.ssl-images-amazon.com/images/I/51Ukxxbo-mL._SX359_BO1,204,203,200_.jpg"
+    }]
+  }
+
+  success ? setTimeout(() => resolve(response), 50) : reject('Failed');
+})
 
 /**
  * @var {array} data an array of graphic novels
@@ -50,6 +74,17 @@ let data; // Do not change this line.
  * Call on the "getBooksApi()" here and set the variable "data" above
  * to the array of graphic novels that is in your promise's response.
  */
+
+// Using .then() and .catch()
+getBooksApi()
+  .then(response => data = response.data)
+  .catch(error => console.log(error));
+
+// // Using async/await
+// (async function() {
+//   const myBooks = await getBooksApi();
+//   data = myBooks.data
+// })();
 
 module.exports = {
   getBooksApi,
