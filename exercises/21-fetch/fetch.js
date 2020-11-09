777768d828910183a21113aa27ad99f2880c8d86
Sean Russell <seanrussellphotography@gmail.com>
2020-11-09 17:37:03 -0500

    fetch hw

--- exercises/21-fetch/fetch.js
+++ exercises/21-fetch/fetch.js
@@ -1,4 +1,5 @@
 (function () {
+
   /**
    * As a user, I should be able to click on the a button to see random dog images.
    * Please use fetch in this example.
@@ -12,4 +13,16 @@
    *    "message": "https://images.dog.ceo/breeds/poodle-toy/n02113624_9550.jpg"
    * }
    */
+
+  // Fetch a random dogpic and change the img in HTML
+  const changeDogPic = () => {
+    fetch("https://dog.ceo/api/breeds/image/random", {method: 'GET'})
+      .then(response => response.json())
+      .then(json => json.message)
+      .then(url => document.querySelector("#image").src = url)
+  }
+  
+  // Add event listener to button
+  document.querySelector("#randomBtn").addEventListener("click", changeDogPic)
+
 })();
